37bbb2574b9068f149471cfe36a636c16853450e
alef1mil <153133594+alef1mil@users.noreply.github.com>
2025-01-18 13:02:09 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,12 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname,'public' ,'index.html'));
 })
 
-app.get("/brasil-teve-aumento-de-38mil-de-assassinatos", (req, res) => {
-    
+app.get("/globais/noticias", (req, res) => {
+    res.sendFile(path.join(__dirname,'public' ,'noticias.html'));
+})
+
+app.get("/globais/noticias/familha-com-membros-eleitos-em-2024-perderao-benefios-com-bolsa-familha", (req, res) => {
+    res.sendFile(path.join(__dirname,'public' ,'not', "jklp.html"));
 })
 
 app.listen(3000, () => {
